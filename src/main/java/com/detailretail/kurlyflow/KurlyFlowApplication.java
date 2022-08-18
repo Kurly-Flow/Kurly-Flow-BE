b1e6d4029a4981df17ce913b3ff67d68fed192ba
dhkstnaos <dhkstnaos@gmail.com>
2022-08-18 17:13:24 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/detailretail/kurlyflow/KurlyFlowApplication.java
@@ -0,0 +1,13 @@
+package com.detailretail.kurlyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/detailretail/kurlyflow/KurlyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detailretail.kurlyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
