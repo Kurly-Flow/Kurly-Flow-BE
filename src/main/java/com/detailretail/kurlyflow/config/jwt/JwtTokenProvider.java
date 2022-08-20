035da48bc45cffd7979e1fabae32dbe57df2f3ed
dhkstnaos <dhkstnaos@gmail.com>
2022-08-21 02:25:29 +0900

    feat: jwt 생성

---
+++ src/main/java/com/detailretail/kurlyflow/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package com.detailretail.kurlyflow.config.jwt;
+
+import com.detailretail.kurlyflow.worker.command.application.WorkerDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+  private final WorkerDetailService workerDetailService;
+  private String secretKey = "llshlllshlllshlllshl";
+  // 토큰 유효시간 30분
+  private long tokenValidTime = 30 * 60 * 1000L;
+
+  @PostConstruct
+  protected void init() {
+    secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+  }
+
+  public String createToken(String userPk, List<String> roles) {
+    Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
+    claims.put("roles", roles); // 정보는 key / value 쌍으로 저장된다.
+    System.out.println("roles.get(0) = " + roles.get(0));
+    Date now = new Date();
+    return Jwts.builder().setClaims(claims)
+        .setIssuedAt(now)
+        .setExpiration(new Date(now.getTime() + tokenValidTime))
+        .signWith(SignatureAlgorithm.HS256, secretKey)
+        .compact();
+  }
+
+  public Authentication getAuthentication(String token) {
+    System.out.println("getAuthentication::"+this.getUserPk(token));
+    UserDetails userDetails = workerDetailService.loadUserByUsername(this.getUserPk(token));
+    return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+  }
+
+  public String getUserPk(String token) {
+    return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+  }
+
+  public String resolveToken(HttpServletRequest request) {
+    return request.getHeader("Authorization");
+  }
+
+  public boolean validateToken(String jwtToken) {
+    try {
+      Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+      return !claims.getBody().getExpiration().before(new Date());
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
