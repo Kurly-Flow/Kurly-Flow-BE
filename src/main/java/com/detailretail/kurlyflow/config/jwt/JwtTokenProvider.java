ec13b34e805523bd4cebc89f9715d0e5f9577a2c
dhkstnaos <dhkstnaos@gmail.com>
2022-08-21 02:59:54 +0900

    refactor: JwtTokenProvider 수정

--- src/main/java/com/detailretail/kurlyflow/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/detailretail/kurlyflow/config/jwt/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.detailretail.kurlyflow.config.jwt;
 
+import com.detailretail.kurlyflow.common.vo.Authority;
 import com.detailretail.kurlyflow.worker.command.application.WorkerDetailService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
@@ -11,6 +12,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,9 +23,10 @@ import org.springframework.stereotype.Component;
 public class JwtTokenProvider {
 
   private final WorkerDetailService workerDetailService;
-  private String secretKey = "llshlllshlllshlllshl";
-  // 토큰 유효시간 30분
-  private long tokenValidTime = 30 * 60 * 1000L;
+  @Value("${jwt.secretKey}")
+  private String secretKey;
+  // 토큰 유효시간 하루
+  private long tokenValidTime = 24 * 60 * 60 * 1000L;
 
   @PostConstruct
   protected void init() {
@@ -33,23 +36,34 @@ public class JwtTokenProvider {
   public String createToken(String userPk, List<String> roles) {
     Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
     claims.put("roles", roles); // 정보는 key / value 쌍으로 저장된다.
-    System.out.println("roles.get(0) = " + roles.get(0));
     Date now = new Date();
-    return Jwts.builder().setClaims(claims)
-        .setIssuedAt(now)
+    return Jwts.builder().setClaims(claims).setIssuedAt(now)
         .setExpiration(new Date(now.getTime() + tokenValidTime))
-        .signWith(SignatureAlgorithm.HS256, secretKey)
-        .compact();
+        .signWith(SignatureAlgorithm.HS256, secretKey).compact();
   }
 
   public Authentication getAuthentication(String token) {
-    System.out.println("getAuthentication::"+this.getUserPk(token));
-    UserDetails userDetails = workerDetailService.loadUserByUsername(this.getUserPk(token));
+    UserDetails userDetails = findUserDetails(token);
     return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
   }
 
-  public String getUserPk(String token) {
+  public String getId(String token) {
     return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+  }
+
+  public List<String> getRoles(String token) {
+    return (List<String>) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody()
+        .get("roles");
+  }
+
+  public UserDetails findUserDetails(String token) {
+    Authority authority = Authority.valueOf(this.getRoles(token).get(0));
+    if (authority.equals(Authority.ROLE_WORKER)) {
+      return workerDetailService.loadUserByUsername(this.getId(token));
+    } else {
+      //관리자
+      return null;
+    }
   }
 
   public String resolveToken(HttpServletRequest request) {
