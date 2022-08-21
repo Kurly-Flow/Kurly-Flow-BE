09b3654c172415ee581ed2890ae2940c9fc38200
dhkstnaos <dhkstnaos@gmail.com>
2022-08-21 23:05:24 +0900

    refactor: 하루 전 이내로 일한 작업자들을 workerHistory로 저장

--- src/main/java/com/detailretail/kurlyflow/worker/command/application/WorkerHistoryService.java
+++ src/main/java/com/detailretail/kurlyflow/worker/command/application/WorkerHistoryService.java
@@ -3,9 +3,7 @@ package com.detailretail.kurlyflow.worker.command.application;
 import com.detailretail.kurlyflow.worker.command.domain.Worker;
 import com.detailretail.kurlyflow.worker.command.domain.WorkerHistory;
 import com.detailretail.kurlyflow.worker.command.domain.WorkerRepository;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -22,8 +20,7 @@ public class WorkerHistoryService {
   @Scheduled(cron = "0 0 5 * * *") //새벽 5시에 상태 변경
   public void saveWorkerHistory() {
     List<Worker> workers = workerRepository.findAllByIsWorkedTrueAndLoginAtBetween(
-        LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(0, 0, 0)),
-        LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59)));
+        LocalDateTime.now().minusDays(1L), LocalDateTime.now());
     workers.stream()
         .forEach(worker -> worker.addHistory(new WorkerHistory(worker.getRegion(), worker)));
   }
