b7aa49a94ad88ea247a28dbaca0c6ba872617744
dhkstnaos <dhkstnaos@gmail.com>
2022-08-22 01:50:45 +0900

    feat: 배치 테이블

---
+++ src/main/java/com/detailretail/kurlyflow/worker/command/domain/Batch.java
@@ -0,0 +1,33 @@
+package com.detailretail.kurlyflow.worker.command.domain;
+
+import com.detailretail.kurlyflow.order.command.domain.InvoiceProduct;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "batch")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Batch {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id")
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "worker_id")
+  private Worker worker;
+
+  @ManyToOne
+  @JoinColumn(name = "invoice_product_id")
+  private InvoiceProduct invoiceProduct;
+}
