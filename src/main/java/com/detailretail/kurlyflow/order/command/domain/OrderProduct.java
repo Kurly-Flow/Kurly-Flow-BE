bd8de77f8022d48dbbb815e2d6360a9e99e8d135
dhkstnaos <dhkstnaos@gmail.com>
2022-08-22 00:19:47 +0900

    feat: OrderProduct 엔티티

---
+++ src/main/java/com/detailretail/kurlyflow/order/command/domain/OrderProduct.java
@@ -0,0 +1,36 @@
+package com.detailretail.kurlyflow.order.command.domain;
+
+import com.detailretail.kurlyflow.product.command.domain.Product;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_product")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderProduct {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id")
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "invoice_id")
+  private Invoice invoice;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  private Integer quantity;
+
+}
