5319d914d1c03330527e6990c86e628911ecc40e
dhkstnaos <dhkstnaos@gmail.com>
2022-08-22 02:26:13 +0900

    feat: 멀티피킹리스트 & 바코드 입력

--- src/main/java/com/detailretail/kurlyflow/worker/presentation/PickingController.java
+++ src/main/java/com/detailretail/kurlyflow/worker/presentation/PickingController.java
@@ -4,11 +4,15 @@ import com.detailretail.kurlyflow.config.aop.CurrentUser;
 import com.detailretail.kurlyflow.worker.command.application.LoginRequest;
 import com.detailretail.kurlyflow.worker.command.application.LoginResponse;
 import com.detailretail.kurlyflow.worker.command.application.LoginService;
+import com.detailretail.kurlyflow.worker.command.application.MultiBatchResponse;
+import com.detailretail.kurlyflow.worker.command.application.PickingService;
 import com.detailretail.kurlyflow.worker.command.domain.CustomWorkerDetails;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +24,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class PickingController {
 
   private final LoginService loginService;
+  private final PickingService pickingService;
 
   @PostMapping("/login")
   public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-    LoginResponse loginResponse = loginService.login(loginRequest);
+    LoginResponse loginResponse = loginService.startWork(loginRequest);
     return ResponseEntity.ok(loginResponse);
   }
 
   @PreAuthorize("hasRole('WORKER')")
   @GetMapping("/multi")
-  public ResponseEntity<Void> multiPickingList(@CurrentUser CustomWorkerDetails worker) {
+  public ResponseEntity<List<MultiBatchResponse>> multiPickingList(
+      @CurrentUser CustomWorkerDetails worker) {
+    List<MultiBatchResponse> multiPickingList = pickingService.getMultiPickingList(worker.getId());
+    return ResponseEntity.ok(multiPickingList);
+  }
+
+  @PreAuthorize("hasRole('WORKER')")
+  @GetMapping("/bacord/{batchId}")
+  public ResponseEntity<Void> readBarcord(@PathVariable("batchId") Long batchId) {
+    pickingService.readBarcord(batchId);
     return ResponseEntity.ok(null);
   }
 }
