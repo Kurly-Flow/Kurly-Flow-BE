cf975a8ebdf36ac444dfdb91090e4ac7aafe198d
dhkstnaos <dhkstnaos@gmail.com>
2022-08-22 18:38:28 +0900

    feat: InvoiceRepository

---
+++ src/main/java/com/detailretail/kurlyflow/order/command/domain/InvoiceRepository.java
@@ -0,0 +1,12 @@
+package com.detailretail.kurlyflow.order.command.domain;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+  @Query("SELECT i FROM Invoice i JOIN FETCH i.invoiceProducts ip JOIN FETCH ip.product" )
+  Optional<Invoice> getInvoice(@Param(value = "invoiceId") Long invoiceId);
+}
