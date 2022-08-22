465344f96bba1f3ef39501d7302d23eefa0f9b5b
dhkstnaos <dhkstnaos@gmail.com>
2022-08-22 17:10:28 +0900

    feat: 토트 엔티티

--- src/main/java/com/detailretail/kurlyflow/worker/command/domain/Tote.java
+++ src/main/java/com/detailretail/kurlyflow/worker/command/domain/Tote.java
@@ -1,9 +1,14 @@
 package com.detailretail.kurlyflow.worker.command.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -21,4 +26,7 @@ public class Tote {
   private Long id;
 
   private String name;
+
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "tote")
+  private List<Batch> batches = new ArrayList<>();
 }
