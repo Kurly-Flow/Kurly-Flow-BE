3b9bcc4e43bc0b657913c52ee427687ab29b1266
dhkstnaos <dhkstnaos@gmail.com>
2022-08-23 03:14:01 +0900

    feat: end controller

--- src/main/java/com/detailretail/kurlyflow/basket/presentation/EndController.java
+++ src/main/java/com/detailretail/kurlyflow/basket/presentation/EndController.java
@@ -1,12 +1,16 @@
-package com.detailretail.kurlyflow.order.presentation;
+package com.detailretail.kurlyflow.basket.presentation;
 
-import com.detailretail.kurlyflow.order.query.application.EndService;
+import com.detailretail.kurlyflow.basket.query.application.EndBasketInvoiceResponse;
+import com.detailretail.kurlyflow.basket.query.application.EndCompleteResponse;
+import com.detailretail.kurlyflow.basket.query.application.EndService;
 import com.detailretail.kurlyflow.config.aop.CurrentUser;
 import com.detailretail.kurlyflow.worker.command.domain.CustomWorkerDetails;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +23,17 @@ public class EndController {
 
   @PreAuthorize("hasRole('WORKER')")
   @GetMapping
-  public ResponseEntity<Void> getInvoices(@CurrentUser CustomWorkerDetails worker) {
-    endService.getInvoices(worker.getId());
-    return ResponseEntity.ok(null);
+  public ResponseEntity<List<EndCompleteResponse>> getEndStatus(
+      @CurrentUser CustomWorkerDetails worker) {
+    List<EndCompleteResponse> endCompleteResponses = endService.getEndStatus(worker.getId());
+    return ResponseEntity.ok(endCompleteResponses);
+  }
+
+  @PreAuthorize("hasRole('WORKER')")
+  @GetMapping("/{invoiceId}")
+  public ResponseEntity<List<EndBasketInvoiceResponse>> getInvoice(
+      @PathVariable("invoiceId") Long invoiceId) {
+    List<EndBasketInvoiceResponse> endBasketInvoiceResponses = endService.getInvoice(invoiceId);
+    return ResponseEntity.ok(endBasketInvoiceResponses);
   }
 }
