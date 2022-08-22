4b0060f16153025e006ef5b7f2d5b97442fb5943
dhkstnaos <dhkstnaos@gmail.com>
2022-08-22 17:10:54 +0900

    feat: 피킹 컨트롤러

--- src/main/java/com/detailretail/kurlyflow/worker/exception/EntityNotFoundException.java
+++ src/main/java/com/detailretail/kurlyflow/worker/exception/EntityNotFoundException.java
@@ -3,11 +3,11 @@ package com.detailretail.kurlyflow.worker.exception;
 
 import com.detailretail.kurlyflow.common.exception.NotFoundException;
 
-public class WorkerNotFoundException extends NotFoundException {
+public class EntityNotFoundException extends NotFoundException {
 
-  private static final String WORKER_NOT_FOUND = "해당 작업자가 존재하지 않습니다.";
+  private static final String ENTITY_NOT_FOUND = "해당 엔티티가 존재하지 않습니다.";
 
-  public WorkerNotFoundException  () {
-    super(WORKER_NOT_FOUND);
+  public EntityNotFoundException() {
+    super(ENTITY_NOT_FOUND);
   }
 }
--- src/main/java/com/detailretail/kurlyflow/worker/presentation/PickingController.java
+++ src/main/java/com/detailretail/kurlyflow/worker/presentation/PickingController.java
@@ -2,11 +2,12 @@ package com.detailretail.kurlyflow.worker.presentation;
 
 import com.detailretail.kurlyflow.config.aop.CurrentUser;
 import com.detailretail.kurlyflow.worker.command.application.LoginRequest;
-import com.detailretail.kurlyflow.worker.command.application.LoginService;
-import com.detailretail.kurlyflow.worker.command.application.MultiBatchResponse;
 import com.detailretail.kurlyflow.worker.command.application.PickingService;
 import com.detailretail.kurlyflow.worker.command.application.WorkingPlaceLoginResponse;
 import com.detailretail.kurlyflow.worker.command.domain.CustomWorkerDetails;
+import com.detailretail.kurlyflow.worker.query.application.BatchService;
+import com.detailretail.kurlyflow.worker.query.application.MultiBatchResponse;
+import com.detailretail.kurlyflow.worker.query.application.ToteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,8 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PickingController {
 
-  private final LoginService loginService;
   private final PickingService pickingService;
+  private final BatchService batchService;
+  private final ToteService toteService;
 
   @PostMapping("/login")
   public ResponseEntity<WorkingPlaceLoginResponse> login(@RequestBody LoginRequest loginRequest) {
@@ -35,14 +38,15 @@ public class PickingController {
   @GetMapping("/multi")
   public ResponseEntity<MultiBatchResponse> multiPickingList(
       @CurrentUser CustomWorkerDetails worker) {
-    MultiBatchResponse multiPickingList = pickingService.getMultiPickingList(worker.getId());
+    MultiBatchResponse multiPickingList = batchService.getMultiPickingList(worker.getId());
     return ResponseEntity.ok(multiPickingList);
   }
 
   @PreAuthorize("hasRole('WORKER')")
   @GetMapping("/bacord/{batchId}")
-  public ResponseEntity<Void> readBarcode(@PathVariable("batchId") Long batchId) {
-    pickingService.readBarcode(batchId);
+  public ResponseEntity<Void> readBarcode(@PathVariable("batchId") Long batchId,
+      @RequestParam(name = "toteId") Long toteId) {
+    pickingService.readBarcode(batchId, toteId);
     return ResponseEntity.ok(null);
   }
 
@@ -51,5 +55,12 @@ public class PickingController {
   public ResponseEntity<Void> workingToggle(@CurrentUser CustomWorkerDetails worker) {
     pickingService.workingToggle(worker.getId());
     return ResponseEntity.ok(null);
+  }
+
+  @PreAuthorize("hasRole('WORKER')")
+  @GetMapping("/tote")
+  public ResponseEntity<Long> getNewTote(@CurrentUser CustomWorkerDetails worker) {
+    Long toteId = toteService.getTote(worker.getId());
+    return ResponseEntity.ok(toteId);
   }
 }
