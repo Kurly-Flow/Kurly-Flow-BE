a2f8e882d386eeed4aa8c88be1fc948aa5504379
dhkstnaos <dhkstnaos@gmail.com>
2022-08-23 03:13:10 +0900

    fix: AdminRepository 수정

--- src/main/java/com/detailretail/kurlyflow/admin/command/domain/AdminRepository.java
+++ src/main/java/com/detailretail/kurlyflow/admin/command/domain/AdminRepository.java
@@ -1,11 +1,11 @@
 package com.detailretail.kurlyflow.admin.command.domain;
 
 import com.detailretail.kurlyflow.common.vo.EmployeeNumber;
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByEmployeeNumber(EmployeeNumber employeeNumber);
