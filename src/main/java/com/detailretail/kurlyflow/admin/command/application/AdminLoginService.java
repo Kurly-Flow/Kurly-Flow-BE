a3f21271e1a694afa8c54b25ddda23ab306d0492
dhkstnaos <dhkstnaos@gmail.com>
2022-08-24 15:41:25 +0900

    fix: 오류 해결

--- src/main/java/com/detailretail/kurlyflow/admin/command/application/AdminLoginService.java
+++ src/main/java/com/detailretail/kurlyflow/admin/command/application/AdminLoginService.java
@@ -4,6 +4,7 @@ import com.detailretail.kurlyflow.admin.command.domain.Admin;
 import com.detailretail.kurlyflow.admin.command.domain.AdminRepository;
 import com.detailretail.kurlyflow.admin.exception.AdminNotFoundException;
 import com.detailretail.kurlyflow.admin.util.AdminConverter;
+import com.detailretail.kurlyflow.common.vo.Authority;
 import com.detailretail.kurlyflow.common.vo.EmployeeNumber;
 import com.detailretail.kurlyflow.config.jwt.JwtTokenProvider;
 import java.util.List;
@@ -16,15 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AdminLoginService {
 
-    private final AdminRepository adminRepository;
-    private final JwtTokenProvider jwtTokenProvider;
+  private final AdminRepository adminRepository;
+  private final JwtTokenProvider jwtTokenProvider;
 
 
-    public AdminLoginResponse adminLogin(AdminLoginRequest adminLoginRequest){
-        Admin admin = adminRepository.findByEmployeeNumber(new EmployeeNumber(adminLoginRequest.getEmployeeNumber()))
-                .orElseThrow(AdminNotFoundException::new);
-        admin.matchPassword(adminLoginRequest.getPassword());
-        return AdminConverter.ofAdminLogin(jwtTokenProvider.createToken(String.valueOf(admin.getId()),
-                List.of(admin.getAuthority().name())), admin.getName());
-    }
+  public AdminLoginResponse adminLogin(AdminLoginRequest adminLoginRequest) {
+    Admin admin = adminRepository.findByEmployeeNumber(
+            new EmployeeNumber(adminLoginRequest.getEmployeeNumber()))
+        .orElseThrow(AdminNotFoundException::new);
+    admin.matchPassword(adminLoginRequest.getPassword());
+    return AdminConverter.ofAdminLogin(jwtTokenProvider.createToken(String.valueOf(admin.getId()),
+        List.of(Authority.ROLE_ADMIN.name())), admin.getName());
+  }
 }
